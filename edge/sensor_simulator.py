4305aeae4a0a553362b7b47a8849a5b8daa8c948
bbact <133502659+bbact@users.noreply.github.com>
2026-01-14 11:42:30 +0800

    Update sensor_simulator.py

--- edge/sensor_simulator.py
+++ edge/sensor_simulator.py
@@ -1,8 +1,16 @@
 import time
 import json
 import random
+from kafka import KafkaProducer
 
 DEVICE_ID = "device-001"
+KAFKA_TOPIC = "sensor-data"
+KAFKA_SERVER = "localhost:9092"
+
+producer = KafkaProducer(
+    bootstrap_servers=KAFKA_SERVER,
+    value_serializer=lambda v: json.dumps(v).encode("utf-8")
+)
 
 def generate_sensor_data():
     return {
@@ -14,5 +22,6 @@ def generate_sensor_data():
 if __name__ == "__main__":
     while True:
         data = generate_sensor_data()
-        print(json.dumps(data))
+        producer.send(KAFKA_TOPIC, data)
+        print(f"send to kafka: {data}")
         time.sleep(2)
